86ab1fd084cf7285505750571565084f0c0ca94d
hucl <hucl@hundsun.com>
2017-06-28 18:59:19 +0800

    上传vue

---
+++ vue/src/router/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Todo from '@/components/Todo'
+import App from '../App'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+  ]
+})
